ab8338294894956b4fe5f95044d1940582aea691
fboltz <frederic.boltz@gmail.com>
2022-12-30 14:41:49 +0100

    TYPE: Use good variable

--- bin/create-masterkube.sh
+++ bin/create-masterkube.sh
@@ -1969,7 +1969,7 @@ done
 if [ "${USE_NLB}" != "YES" ]; then
     # Register in godaddy IP addresses point in public IP
     if [ ! -z ${GODADDY_API_KEY} ]; then
-        curl -s -X PUT "https://api.godaddy.com/v1/domains/${DOMAIN_NAME}/records/A/${MASTERKUBE}" \
+        curl -s -X PUT "https://api.godaddy.com/v1/domains/${PUBLIC_DOMAIN_NAME}/records/A/${MASTERKUBE}" \
             -H "Authorization: sso-key ${GODADDY_API_KEY}:${GODADDY_API_SECRET}" \
             -H "Content-Type: application/json" \
             -d "${GODADDY_REGISTER}"
